05864e9aa2c3cc4dd039ca8be2f87167c791b5e4
Saprem <134441518+sapython12@users.noreply.github.com>
2025-08-19 17:30:35 +0530

    Create exercise3.py

---
+++ exercise3.py
@@ -0,0 +1,26 @@
+month = int(input("enter month number (1-12): "))
+
+if month == 1:
+    print("january")
+if month == 2:
+    print("february")
+if month == 3:
+    print("march")
+if month == 4:
+    print("april")
+if month == 5:
+    print("may")
+if month == 6:
+    print("june")
+if month == 7:
+    print("july")
+if month == 8:
+    print("august")
+if month == 9:
+    print("september")
+if month == 10:
+    print("october")
+if month == 11:
+    print("november")
+if month == 12:
+    print("december")
