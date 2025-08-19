8251593863a0ca102a1061baa717c2fb9d6dbeca
Saprem <134441518+sapython12@users.noreply.github.com>
2025-08-19 17:29:57 +0530

    Create exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,9 @@
+age1 = int(input("enter age of first brother: "))
+age2 = int(input("enter age of second brother: "))
+
+if age1 > age2:
+    print("first brother is elder")
+if age2 > age1:
+    print("second brother is elder")
+if age1 == age2:
+    print("both are of same age")
